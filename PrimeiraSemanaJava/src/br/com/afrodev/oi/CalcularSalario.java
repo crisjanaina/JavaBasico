84d94b71a4157d9645e5b13eeec50041cba55ade
crisjanaina <crisjanainamm@gmail.com>
2020-11-11 17:42:16 -0300

    último exercício da aula 2

--- PrimeiraSemanaJava/src/br/com/afrodev/oi/CalcularSalario.java
+++ PrimeiraSemanaJava/src/br/com/afrodev/oi/CalcularSalario.java
@@ -1,5 +1,19 @@
 package br.com.afrodev.oi;
 
+import java.text.DecimalFormat;
+
 public class CalcularSalario {
+	
+	public static void main(String[] args) {
+		
+		float salariobruto = 2530; 
+		float vendasDoMes = 3000;
+		
+		float comissaoMes = vendasDoMes * 1/10;
+		float salarioTotal = salariobruto + comissaoMes;
+		DecimalFormat df =  new DecimalFormat("0.00");
+		System.out.println("O salário total do funcionário é: " + df.format(salarioTotal));
+		 
+	}
 
 }
