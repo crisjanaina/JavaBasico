efa9eb0c50505e75fdbae28bff53c344a147e37c
crisjanaina <crisjanainamm@gmail.com>
2020-11-12 20:28:32 -0300

    aula 3

--- PrimeiraSemanaJava/src/br/com/afrodev/oi/Main.java
+++ PrimeiraSemanaJava/src/br/com/afrodev/oi/Main.java
@@ -1,25 +1,53 @@
 package br.com.afrodev.oi;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		Calculadora c = new Calculadora();
 		
-		System.out.println(c.realizarMultiplicacao(10));
+		//System.out.println(c.realizarMultiplicacao(10));
 		
-		System.out.println(c.realizaSoma(10));
+		//System.out.println(c.realizaSoma(10));
 		
-		System.out.println(Calculadora.valorPadraoSomaInteiro);
+		//System.out.println(Calculadora.valorPadraoSomaInteiro);
 		
 		byte b = 100;
-		short s = 32;
-		int i = 30;
-		long l =201;
+		short s = 32; // - 32000 até 32000
+		int i = 30; // - 2147483648 até 2147483648
+		long l = 20l;// - 9223372036854775808 até 9223372036854775808
 		
-		char ch = 30;
+		char ch =  30; // 'a'
 		
-		System.out.println(ch);
+		//System.out.println(ch);
+		
+		
+		
+		float f = 10.3443f; // 6
+		double d = 12.343d; // 15
+		boolean bol = true; // valores true ou false
+		
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nome =sc.nextLine();
+		System.out.println(nome);
+		float numF = sc.nextFloat();
+		System.out.println(numF);
+		int num1 = sc.nextInt();
+		System.out.println(num1);
+		byte byte1 = sc.nextByte();
+		System.out.println(byte1);
+		long lgl = sc.nextLong();
+		System.out.println(lgl);
+		boolean bl = sc.nextBoolean();
+		System.out.println(bl);
+		double num2 = sc.nextDouble();
+		System.out.println(num2);
+		
 		
 	}
 
---
+++ PrimeiraSemanaJava/src/br/com/afrodev/oi/TreinandoScanner.java
@@ -0,0 +1,60 @@
+package br.com.afrodev.oi;
+
+import java.util.Scanner;
+
+public class TreinandoScanner {
+
+	public static void main(String[] args) {
+		/*
+		 * Crie um programa que leia o seu String nome; int idade; float peso; Imprimir
+		 * Meu nome é ... Minha idade é ... Meu peso é ... 
+		 * Realize um teste com o if:
+		 * Caso peso seja maior que 80 imprimir : Vocé precisa correr mais. 
+		 * Caso peso seja menor : Coma mais carboidratos. 
+		 * Adicione toda a cadeia de codigo que
+		 * criamos em uma estrutura while enquanto o valor de controle for "C"
+		 * 
+		 * ++ é o mesmo que somar +1 
+		 * -- é o mesmo que diminuir -1
+		 */
+
+		String controle = "C";
+		Scanner sc = new Scanner(System.in);
+		while (controle.equals("C")) {
+
+			System.out.print("Qual o seu nome? ");
+			String nome = sc.nextLine();
+			System.out.print("Quantos anos você tem? ");
+			int idade = sc.nextInt();
+			System.out.print("Qual o seu peso? ");
+			float peso = sc.nextFloat();
+
+			System.out.printf("\nMeu nome é %s \nMinha idade é %d \nMeu peso é %.2f \n", nome, idade, peso);
+
+		}
+	}
+
+	public static void verificarIdadeEPeso(float peso, int idade, String nome) {
+
+		if ((idade > 80 && peso < 35) || nome.startsWith("Ja")) {
+			System.out.println("Você precisa comer!!!");
+		} else {
+			System.out.println("Pode comer, mas coma com moderação ");
+		}
+
+	}
+
+	public static void verificaIdadeComSwitch(int idade) {
+		switch (idade) {
+		case 80:
+			System.out.println("Você está idoso!");
+			break;
+		case 40:
+			System.out.println("Você está maduro!");
+			break;
+		default:
+			System.out.println("Talvez você seja jovem!");
+			break;
+		}
+	}
+}
