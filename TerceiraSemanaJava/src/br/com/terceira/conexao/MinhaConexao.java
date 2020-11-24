7321dc70555914c7c5cb69485a2c7b622f5ba43b
crisjanaina <crisjanainamm@gmail.com>
2020-11-23 22:35:10 -0300

    Aula dia 23/11 - BD

---
+++ TerceiraSemanaJava/src/br/com/terceira/conexao/MinhaConexao.java
@@ -0,0 +1,43 @@
+package br.com.terceira.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MinhaConexao {
+
+	private static final String SERVIDOR = "localhost";
+	private static final String USUARIO = "root";
+	private static final String PORTA = "3306";
+	private static final String SCHEMA = "bd_afrodev";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String PARAMS = "?serverTimezone=UTC&useSSL=false";
+	private static Connection instance = null;
+
+
+	@SuppressWarnings("finally")
+	public synchronized static Connection getInstance() {
+
+		try {
+			if (instance == null || instance.isClosed()) {
+				instance = conectar();
+			} else if (!instance.isValid(0)) {
+				instance.close();
+				instance = conectar();
+			}
+		} catch (Exception e) {
+			System.err.print(e.toString());
+		} finally {
+			return instance;
+		}
+
+	}
+
+	private static Connection conectar()
+			throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+		Class.forName(DRIVER).newInstance();
+		Connection con = DriverManager.getConnection("jdbc:mysql://" + SERVIDOR + ":" + PORTA + "/" + SCHEMA + PARAMS,
+				USUARIO , "");
+		return con;
+	}
+}
