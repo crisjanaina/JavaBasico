50f605c4f3e6f9c7f03d1606de9ee8c3aa99df40
crisjanaina <crisjanainamm@gmail.com>
2020-11-27 09:30:33 -0300

    Hibernate parte 1

---
+++ afrodev-hibernate/src/main/java/br/com/afrodev_hibernate/afrodev_hibernate/App.java
@@ -0,0 +1,13 @@
+package br.com.afrodev_hibernate.afrodev_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
